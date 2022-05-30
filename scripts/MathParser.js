6c5f6505e507dce7c0a7d5ccb47d5645f3a7ab02
jurako91 <jurako91@gmail.com>
2022-05-30 23:44:20 +0300

    Work on math parser

--- scripts/MathParser.js
+++ scripts/MathParser.js
@@ -1,10 +1,51 @@
 export class MathParser {
-    constructor() {
-        //...
+    constructor(tokens) {
+        this.operatorStack = [];
+        this.outputQueue = new Queue();
+        this.tokens = tokens;
+
+        this.operatorPrecedence = {
+            1: ['*', '/'],
+            2: ['+', '-']
+        }
     }
 
-    class Queue {
+    //readTokens
+    readTokens() {
+        while(this.tokens.length > 0) {
+            let token = this.tokens.unshift();
+
+            if(!isNaN(token)) {
+                this.outputQueue.enqueue(token);
+            } else if(this.isOperator(token)) {
+                //getOperatorPrecedence
+                this.operatorStack
+            } else {
+                throw Error;
+            }
+
+        }
+    }
+
+    //getOperatorPrecendece
+
+    isOperator(token) {
+        return /+-*\//.test(token);
+    }
 
 }
-    
+
+
+class Queue {
+    constructor() {
+        this.queue = [];
+    }
+
+    enqueue(item) {
+        this.queue.unshift(item);
+    }
+
+    dequeue() {
+        this.queue.pop();
+    }
 }
